096843639f6642edfdd68badb4534d091c0338ed
jlstout1998 <jlstout1998@gmail.com>
2026-03-25 18:55:57 -0700

    Update VerticalSpriteProvider.java

--- src/main/java/me/pepperbell/continuity/client/processor/simple/VerticalSpriteProvider.java
+++ src/main/java/me/pepperbell/continuity/client/processor/simple/VerticalSpriteProvider.java
@@ -8,12 +8,12 @@ import me.pepperbell.continuity.client.processor.DirectionMaps;
 import me.pepperbell.continuity.client.processor.OrientationMode;
 import me.pepperbell.continuity.client.processor.ProcessingDataKeys;
 import me.pepperbell.continuity.client.properties.OrientedConnectingCtmProperties;
-import net.fabricmc.fabric.api.renderer.v1.mesh.QuadView;
+import net.fabricmc.fabric.api.client.renderer.v1.mesh.QuadView;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class VerticalSpriteProvider implements SpriteProvider {
