c7bd5f26a32ea68a2e1a6388872785ca95422c67
jlstout1998 <jlstout1998@gmail.com>
2026-03-25 18:57:11 -0700

    Update ProcessingPredicate.java

--- src/main/java/me/pepperbell/continuity/client/processor/ProcessingPredicate.java
+++ src/main/java/me/pepperbell/continuity/client/processor/ProcessingPredicate.java
@@ -1,10 +1,10 @@
 package me.pepperbell.continuity.client.processor;
 
 import me.pepperbell.continuity.api.client.ProcessingDataProvider;
-import net.fabricmc.fabric.api.renderer.v1.mesh.QuadView;
+import net.fabricmc.fabric.api.client.renderer.v1.mesh.QuadView;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 
 public interface ProcessingPredicate {
