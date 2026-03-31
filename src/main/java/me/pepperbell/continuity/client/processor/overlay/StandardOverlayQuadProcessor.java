f7f3ee97ac472c5472be9cf1f4a7a4997790928d
jlstout1998 <jlstout1998@gmail.com>
2026-03-31 12:59:25 -0700

    Update import statements for renderer classes

--- src/main/java/me/pepperbell/continuity/client/processor/overlay/StandardOverlayQuadProcessor.java
+++ src/main/java/me/pepperbell/continuity/client/processor/overlay/StandardOverlayQuadProcessor.java
@@ -19,8 +19,8 @@ import me.pepperbell.continuity.client.util.QuadUtil;
 import me.pepperbell.continuity.client.util.RenderUtil;
 import me.pepperbell.continuity.client.util.SpriteCalculator;
 import me.pepperbell.continuity.client.util.TextureUtil;
-import net.fabricmc.fabric.api.renderer.v1.mesh.MutableQuadView;
-import net.fabricmc.fabric.api.renderer.v1.mesh.QuadEmitter;
+import net.fabricmc.fabric.api.client.renderer.v1.mesh.MutableQuadView;
+import net.fabricmc.fabric.api.client.renderer.v1.mesh.QuadEmitter;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -28,7 +28,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.client.renderer.block.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class StandardOverlayQuadProcessor extends AbstractQuadProcessor {
